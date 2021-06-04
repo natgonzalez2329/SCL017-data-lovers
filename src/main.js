b7a1f7ed86840090896da0d459002d186e62ec1b
GabbyHolics <karenthgabriela@gmail.com>
2021-06-04 14:34:26 -0400

    El slider se ve pero no cambia

--- src/main.js
+++ src/main.js
@@ -4,3 +4,34 @@ import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
 console.log(example, data);
+
+
+let leading = 1;
+viewSlider(leading);
+
+function nextSlider(n){
+viewSlider(leading+=n);
+}
+
+
+function viewSlider(n){
+    let i;
+    let slider = document.getElementsByClassName("my-slider");
+   
+setInterval (function time(){
+    viewSlider(leading + 1)
+},4000);
+
+    if( n > slider.lenght){
+        leading = 1;
+    }
+    if (n < 1){
+        leading= slider.lenght();
+    }
+ for(i = 0; i < slider,length; i++){
+     slider[i].style.display = "none"
+ }
+    
+    slider[leading-1].style.display = "block";
+
+}
