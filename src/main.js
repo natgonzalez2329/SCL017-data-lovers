2a37d0a0150760ec44a366b5ee151b12b385a619
Natasha Gonzalez <82616634+natgonzalez2329@users.noreply.github.com>
2021-06-14 09:43:39 -0300

    css y solución de mutacion de sort

--- src/main.js
+++ src/main.js
@@ -55,7 +55,7 @@ factsBtn.addEventListener("click", () => {
     characters.style.display = "none";
     facts.style.display = "block";
 });
-// clasificarla
+
 const dataRickAndMorty = data.results.map(rickAndMorty => {
     const { id, name, status, species, type, gender, origin, location, image } = rickAndMorty;
     const newDataRickAndMorty = {
@@ -71,7 +71,6 @@ const dataRickAndMorty = data.results.map(rickAndMorty => {
     };
     return newDataRickAndMorty;
 });
- // Leer la data 
 
 dataRickAndMorty.forEach((card) => {
     const {id, name, status, species, type, gender, origin, location, image } = card;
@@ -96,22 +95,22 @@ dataRickAndMorty.forEach((card) => {
     idCharacter.innerHTML = id;
     let nameStatusCharacter = document.createElement("h2");
     nameStatusCharacter.className = "nameStatusCharacter";
-    nameStatusCharacter.innerHTML = name + "-" + status;
+    nameStatusCharacter.innerHTML = name + " - " + status;
     let speciesCharacter = document.createElement("p");
     speciesCharacter.className = "speciesCharacter";
-    speciesCharacter.innerHTML = species;
+    speciesCharacter.innerHTML = "Specie: " + species;
     let typeCharacter = document.createElement("p");
     typeCharacter.className = "typeCharacter";
-    typeCharacter.innerHTML = typeValue;
+    typeCharacter.innerHTML = "Type: " + typeValue;
     let genderCharacter = document.createElement("p");
     genderCharacter.className = "genderCharacter";
-    genderCharacter.innerHTML = gender;
+    genderCharacter.innerHTML = "Gender: " + gender;
     let originCharacter = document.createElement("p");
     originCharacter.className = "originCharacter";
-    originCharacter.innerHTML = originName;
+    originCharacter.innerHTML = "Origin: " + originName;
     let locationCharacter = document.createElement("p");
     locationCharacter.className = "locationCharacter";
-    locationCharacter.innerHTML = locationName;
+    locationCharacter.innerHTML = "Location: " + locationName;
     
     characters.appendChild(containerCharacterCard);
     containerCharacterCard.appendChild(characterCard);
@@ -125,50 +124,52 @@ dataRickAndMorty.forEach((card) => {
     contentCard.appendChild(originCharacter);
     contentCard.appendChild(locationCharacter);   
 });
-// Filtrado
-const filterDataFemale = dataRickAndMorty.filter((genderFilter) =>  genderFilter.gender === "Female");
-const filterDataMale = dataRickAndMorty.filter((genderFilter) =>  genderFilter.gender ===  "Male");
-const filterDataUnknown = dataRickAndMorty.filter((genderFilter) =>  genderFilter.gender === "unknown");
-console.log(filterDataFemale);
-console.log(filterDataMale);
-console.log(filterDataUnknown);
-const filterDataAlive = dataRickAndMorty.filter((statusFilter) =>  statusFilter.status === "Alive");
-const filterDataDead = dataRickAndMorty.filter((statusFilter) =>  statusFilter.status === "Dead");
-const filterDataStatusUnknown = dataRickAndMorty.filter((statusFilter) =>  statusFilter.status === "unknown");
-console.log(filterDataAlive);
-console.log(filterDataDead);
-console.log(filterDataStatusUnknown);
-const filterpecies = dataRickAndMorty.filter((speciesFilter) =>  speciesFilter.species === "Alien");
-const filterDataAnimal = dataRickAndMorty.filter((speciesFilter) =>  speciesFilter.species === "Animal");
-const filterDataCronenberg = dataRickAndMorty.filter((speciesFilter) =>  speciesFilter.species === "Cronenberg");
-const filterDataDisease = dataRickAndMorty.filter((speciesFilter) =>  speciesFilter.species === "Disease");
-const filterDataHuman = dataRickAndMorty.filter((speciesFilter) =>  speciesFilter.species === "Human");
-const filterDataHumanoid  = dataRickAndMorty.filter((speciesFilter) =>  speciesFilter.species === "Humanoid");
-const filterDataMytholog = dataRickAndMorty.filter((speciesFilter) =>  speciesFilter.species === "Mytholog");
-const filterDataParasite = dataRickAndMorty.filter((speciesFilter) =>  speciesFilter.species === "Parasite");
-const filterDataPoopybutthole = dataRickAndMorty.filter((speciesFilter) =>  speciesFilter.species === "Poopybutthole");
-const filterDataRobot = dataRickAndMorty.filter((speciesFilter) =>  speciesFilter.species === "Robot");
-const filterDataunknown = dataRickAndMorty.filter((speciesFilter) =>  speciesFilter.species === "unknown");
-const filterDataStatusVampire = dataRickAndMorty.filter((speciesFilter) =>  speciesFilter.species === "Vampire");
-console.log(filterpecies);
-console.log(filterDataAnimal);
-console.log(filterDataCronenberg);
-console.log(filterDataDisease);
-console.log(filterDataHuman);
-console.log(filterDataHumanoid);
-console.log(filterDataMytholog);
-console.log(filterDataParasite);
-console.log(filterDataPoopybutthole);
-console.log(filterDataRobot);
-console.log(filterDataunknown);
-console.log(filterDataStatusVampire);
-//sortBy
-const sortByName = dataRickAndMorty.sort((a,b) => {
+
+const filterGenderFemale = dataRickAndMorty.filter((genderFilter) =>  genderFilter.gender === "Female");
+const filterGenderMale = dataRickAndMorty.filter((genderFilter) =>  genderFilter.gender ===  "Male");
+const filterGenderUnknown = dataRickAndMorty.filter((genderFilter) =>  genderFilter.gender === "unknown");
+//console.log(filterGenderFemale);
+//console.log(filterGenderMale);
+//console.log(filterGenderUnknown);
+const filterStatusAlive = dataRickAndMorty.filter((statusFilter) =>  statusFilter.status === "Alive");
+const filterStatusDead = dataRickAndMorty.filter((statusFilter) =>  statusFilter.status === "Dead");
+const filterStatusUnknown = dataRickAndMorty.filter((statusFilter) =>  statusFilter.status === "unknown");
+//console.log(filterStatusAlive);
+//console.log(filterStatusDead);
+//console.log(filterStatusUnknown);
+const filterSpeciesAlien = dataRickAndMorty.filter((speciesFilter) =>  speciesFilter.species === "Alien");
+const filterSpeciesAnimal = dataRickAndMorty.filter((speciesFilter) =>  speciesFilter.species === "Animal");
+const filterSpeciesCronenberg = dataRickAndMorty.filter((speciesFilter) =>  speciesFilter.species === "Cronenberg");
+const filterSpeciesDisease = dataRickAndMorty.filter((speciesFilter) =>  speciesFilter.species === "Disease");
+const filterSpeciesHuman = dataRickAndMorty.filter((speciesFilter) =>  speciesFilter.species === "Human");
+const filterSpeciesHumanoid  = dataRickAndMorty.filter((speciesFilter) =>  speciesFilter.species === "Humanoid");
+const filterSpeciesMytholog = dataRickAndMorty.filter((speciesFilter) =>  speciesFilter.species === "Mytholog");
+const filterSpeciesParasite = dataRickAndMorty.filter((speciesFilter) =>  speciesFilter.species === "Parasite");
+const filterSpeciesPoopybutthole = dataRickAndMorty.filter((speciesFilter) =>  speciesFilter.species === "Poopybutthole");
+const filterSpeciesRobot = dataRickAndMorty.filter((speciesFilter) =>  speciesFilter.species === "Robot");
+const filterSpeciesUnknown = dataRickAndMorty.filter((speciesFilter) =>  speciesFilter.species === "unknown");
+const filterSpeciesVampire = dataRickAndMorty.filter((speciesFilter) =>  speciesFilter.species === "Vampire");
+//console.log(filterSpeciesAlien);
+//console.log(filterSpeciesAnimal);
+//console.log(filterSpeciesCronenberg);
+//console.log(filterSpeciesDisease);
+//console.log(filterSpeciesHuman);
+//console.log(filterSpeciesHumanoid);
+//console.log(filterSpeciesMytholog);
+//console.log(filterSpeciesParasite);
+//console.log(filterSpeciesPoopybutthole);
+//console.log(filterSpeciesRobot);
+//console.log(filterSpeciesUnknown);
+//console.log(filterSpeciesVampire);
+
+const dataSortByName = [...dataRickAndMorty];
+const dataSortByLessPopular = [...dataRickAndMorty];
+const sortByName = dataSortByName.sort((a,b) => {
     return (a.name > b.name) ? 1 : -1;
 });
-console.log(sortByName);
-const sortByLessPopular = dataRickAndMorty.sort((a,b) => {
+const sortByLessPopular = dataSortByLessPopular.sort((a,b) => {
     return (a.id < b.id) ? 1 : -1;
 });
+console.log(sortByName);
 console.log(sortByLessPopular);
 
