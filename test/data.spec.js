1a88f2c564f1827c577a8117d6f72762f2cb33d4
Natasha Gonzalez <82616634+natgonzalez2329@users.noreply.github.com>
2021-06-24 09:40:12 -0300

    descripcion test

--- test/data.spec.js
+++ test/data.spec.js
@@ -6,7 +6,7 @@ describe('filterSelect', () => {
   it('is a function', () => {
     expect(typeof filterSelect).toBe('function');
   });
-  it('returns arrayTest', () => {
+  it('should return "[{id: 1, name: "Summer Sanchez", status: "Dead"}, {id: 3, name: "Fabiana Fernandez", status: "Dead"}]" for filterSelect(arrayTest, "status", "Dead")', () => {
     expect(filterSelect(arrayTest, "status", "Dead")).toEqual([{id: 1, name: "Summer Sanchez", status: "Dead"}, {id: 3, name: "Fabiana Fernandez", status: "Dead"}]);
   });
 });
@@ -15,13 +15,13 @@ describe('sortData', () => {
   it('is an object', () => {
     expect(typeof sortData).toBe('object');
   });
-    it('returns arrayTest', () => {
+    it('should return "[{id: 3, name: "Fabiana Fernandez", status: "Dead"}, {id: 2, name: "Nick Perez", status: "Alive"}, {id: 1, name: "Summer Sanchez", status: "Dead"}]" for sortData.sortByAlphabet(arrayTest)', () => {
       expect(sortData.sortByAlphabet(arrayTest)).toEqual([{id: 3, name: "Fabiana Fernandez", status: "Dead"}, {id: 2, name: "Nick Perez", status: "Alive"}, {id: 1, name: "Summer Sanchez", status: "Dead"}]);
   });
-it('returns arrayTest', () => {
+it('should return "[{id: 1, name: "Summer Sanchez", status: "Dead"}, {id: 2, name: "Nick Perez", status: "Alive"}, {id: 3, name: "Fabiana Fernandez", status: "Dead"}]" for sortData.sortByAlphabetReverse(arrayTest)', () => {
   expect(sortData.sortByAlphabetReverse(arrayTest)).toEqual([{id: 1, name: "Summer Sanchez", status: "Dead"}, {id: 2, name: "Nick Perez", status: "Alive"}, {id: 3, name: "Fabiana Fernandez", status: "Dead"}]);    
 });
-it('returns arrayTest', () => {
+it('should return "[{id: 3, name: "Fabiana Fernandez", status: "Dead"}, {id: 2, name: "Nick Perez", status: "Alive"}, {id: 1, name: "Summer Sanchez", status: "Dead"}]" for sortData.sortByLessPopular(arrayTest)', () => {
   expect(sortData.sortByLessPopular(arrayTest)).toEqual([{id: 3, name: "Fabiana Fernandez", status: "Dead"}, {id: 2, name: "Nick Perez", status: "Alive"}, {id: 1, name: "Summer Sanchez", status: "Dead"}]);    
 });
 });
@@ -30,10 +30,10 @@ describe('computeStats', () => {
   it('is an object', () => {
     expect(typeof computeStats).toBe('object');
   });
-  it('returns arrayTest', () => {
+  it('should return "0.61" for computeStats.percentageFilter(arrayTest)', () => {
     expect(computeStats.percentageFilter(arrayTest)).toEqual("0.61");
   });
-  it('returns arrayTest', () => {
+  it('should return "{"propertyKeys": ["Dead", "Alive"], "propertyValues": [2, 1]}" for computeStats.getDataProperty(arrayTest, "status")', () => {
     expect(computeStats.getDataProperty(arrayTest, "status")).toEqual({"propertyKeys": ["Dead", "Alive"], "propertyValues": [2, 1]});
   });
 });
